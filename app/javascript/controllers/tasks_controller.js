2f74c7bafa7c74edfbdaa9ec1e3bc5429ab98b8d
aditya-suchak <adityasuchak97@gmail.com>
2023-06-26 20:54:01 +0530

    Add js function to toggle the checkbox

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -4,4 +4,25 @@ export default class extends Controller {
     connect() {
         console.log(this.element)
     }
+
+    toggle(e) {
+        const id = e.target.dataset.id
+        const csrfToken = document.querySelector("[name='csrf-token']").content
+
+        fetch(`/tasks/${id}/toggle`, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-Token': csrfToken
+            },
+            body: JSON.stringify({ completed: e.target.checked }) // body data type must match "Content-Type" header
+        })
+          .then(response => response.json())
+          .then(data => {
+             alert(data.message)
+           })
+    }
 }
