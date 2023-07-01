2666aa7e2a289d1e7eba821c41caae3f41c4540b
aditya-suchak <adityasuchak97@gmail.com>
2023-07-01 19:04:25 +0530

    Add update and delete task feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,28 @@ class TasksController < ApplicationController
     #   else
     #     format.html { render :new, status: :unprocessable_entity }
     #   end
-    end
+    # end
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "Post was successfully deleted."
   end
 
   private
